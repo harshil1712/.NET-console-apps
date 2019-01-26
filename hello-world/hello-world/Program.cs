61776136b7661826c3a7d5d16bdec4b69e9435bb
Harshil Agrawal <ghagrawal17@gmail.com>
2019-01-26 23:00:30 +0530

    Hello world app

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Harshil Agrawal";
+            String enroll = "160410107001";
+            Console.WriteLine("Name: "+name);
+            Console.WriteLine("Enrollment No.: "+enroll);
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
