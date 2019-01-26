7dc7ee5ce8b85c3ba241b40114586d2250bf5ee0
Harshil Agrawal <ghagrawal17@gmail.com>
2019-01-26 23:10:24 +0530

    box-pattern

---
+++ practical-2-patterns/box-pattern/box-pattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace box_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
