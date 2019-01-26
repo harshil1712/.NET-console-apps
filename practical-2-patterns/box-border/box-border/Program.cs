bb6e7e39df71272bd034b434c7bc98ee4b0118f2
Harshil Agrawal <ghagrawal17@gmail.com>
2019-01-26 23:30:25 +0530

    box border pattern

---
+++ practical-2-patterns/box-border/box-border/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace box_border
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if (i == 0 || i == 4)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        if(j ==0 || j==4)
+                        {
+                            Console.Write("#");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                        
+                    }
+                }
+                Console.WriteLine();
+                
+            }
+            Console.Read();
+        }
+    }
+}
