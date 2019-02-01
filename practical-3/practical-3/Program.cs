4fa6fb5b386beb559db90c250b88da83fe6d26bb
Harshil Agrawal <ghagrawal17@gmail.com>
2019-02-01 18:50:27 +0530

    print message

---
+++ practical-3/practical-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userName = "";
+            String countryName = "";
+            Console.WriteLine("Enter your name:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter your country:");
+            countryName = Console.ReadLine();
+            Console.WriteLine("Hello "+userName+" from country "+countryName+"!");
+            Console.ReadKey();
+        }
+    }
+}
